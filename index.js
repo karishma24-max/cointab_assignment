5be58338f810359dceb7694292f5cd6d71ee032b
karishma24-max <chauhankarishma824@gmail.com>
2023-02-27 02:30:51 +0530

    added

---
+++ index.js
@@ -0,0 +1,103 @@
+require("dotenv").config();
+const express = require('express');
+const connect = require("./Config/db.js");
+const server = express();
+const cors = require('cors');
+const { default: axios } = require("axios");
+server.use(express.json());
+server.use(cors());
+const User=require("./user/user.modal")
+
+
+
+
+
+server.get("/",async (req,res)=>{
+    let responseji= await axios.get("https://randomuser.me/api/?results=50")
+    let data = responseji.data.results
+let result= await User.find()
+console.log(result)
+if(result.length>0)
+{
+    res.send("sorry ,data already exist")
+}
+else{
+    let user= User.insertMany(data)
+      res.send(data)
+}
+   
+})
+
+server.delete("/deleted",async(req,res)=>{
+let data= await User.remove()
+
+    res.send(data)
+
+
+})
+
+server.get("/userdetail",async(req,res)=>{
+    const {page,limit,gender,age}=req.query
+    
+    try{
+        let str =""
+        let datanew= await User.count()
+        
+        res.send(datanew+" ")
+    }
+    catch(e)
+    {
+        res.send(e.message)
+    }
+   
+    // //  if(gender &&age)
+    // // {
+    // //     if(age=="greater")
+    // //     {
+    // // var data=await User.find({gender:gender,"dob.age":{$gt:50}}).skip(skip).limit(limit)
+    // //     }
+    // //     else
+    // //     {
+    // //         var data=await User.find({gender:gender,"dob.age":{$lt:50}}).skip(skip).limit(limit)    
+    // //     }
+    // // }
+    //  if(age)
+    // {
+       
+    //     if(age=="greater")
+    //     {
+    // var data=await User.find({"dob.age":{$gt:50}}).skip(skip).limit(limit)
+    //     }
+    //     else if (age=="greaterji")
+    //     {
+    //         var data=await User.find({"dob.age":{$gte:30}}).skip(skip).limit(limit)  
+    //     }
+    //     else
+    //     {
+    //         var data=await User.find({"dob.age":{$lt:50}}).skip(skip).limit(limit)    
+    //     } 
+    // }
+    // else if(gender)
+    // {
+    //     var data=await User.find({gender:gender}).skip(skip).limit(limit)  
+    // }
+   
+    // else
+    // {
+    //     var data=await User.find().skip(skip).limit(limit)   
+    // }
+    
+    // //console.log(dataji)
+    // res.send(data)
+})
+
+server.listen(8080,async(req,res)=>{
+   try{
+    await connect();
+    console.log(`Database connected`)
+   }
+   catch(e){
+    console.log(e)
+   }
+   console.log(`server listening at port ${8080}`)
+});
